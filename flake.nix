923240677680a8086452feed35c744eecc47b2ca
KFears <kfearsoff@gmail.com>
2023-08-13 18:34:53 +0400

    build: use flake-parts for building

--- flake.nix
+++ flake.nix
@@ -3,55 +3,65 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
-    dream2nix.url = "github:nix-community/dream2nix";
-    dream2nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    dream2nix = {
+      url = "github:nix-community/dream2nix/legacy";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    devenv,
-    systems,
-    dream2nix,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in
-    (dream2nix.lib.makeFlakeOutputs {
-      systems = ["x86_64-linux"];
-      config.projectRoot = ./.;
-      source = ./.;
-      projects = ./projects.toml;
-    })
-    // {
-      devShells =
-        forEachSystem
-        (system: let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                languages.rust.enable = true;
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+        inputs.dream2nix.flakeModuleBeta
+      ];
 
-                pre-commit.hooks = {
-                  # Nix
-                  alejandra.enable = true;
-                  deadnix.enable = true;
-                  statix.enable = true;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-                  # Rust
-                  cargo-check.enable = true;
-                  clippy = {
-                    enable = true;
-                    raw.verbose = true;
-                    raw.args = ["-D" "clippy::pedantic" "-D" "clippy::nursery" "-D" "clippy::unwrap_used" "-D" "clippy::expect_used"];
-                  };
-                  rustfmt.enable = true;
-                };
-              }
-            ];
+      dream2nix = {
+        config.projectRoot = ./.;
+      };
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        devenv.shells.default = {
+          languages.rust.enable = true;
+
+          # https://github.com/cachix/devenv/issues/528
+          containers = pkgs.lib.mkForce {};
+
+          pre-commit.hooks = {
+            # Nix
+            alejandra.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+
+            # Rust
+            cargo-check.enable = true;
+            clippy = {
+              enable = true;
+              raw.verbose = true;
+              raw.args = ["-D" "clippy::pedantic" "-D" "clippy::nursery" "-D" "clippy::unwrap_used" "-D" "clippy::expect_used"];
+            };
+            rustfmt.enable = true;
           };
-        });
+        };
+
+        dream2nix.inputs."tailforward" = {
+          source = ./.;
+          projects = builtins.fromTOML (builtins.readFile ./projects.toml);
+        };
+
+        inherit (config.dream2nix.outputs."tailforward") packages;
+      };
     };
 }
