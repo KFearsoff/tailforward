0d315d7b8e1b80172e825c3a1d1290bb7838b7e3
KFears <kfearsoff@gmail.com>
2023-08-13 19:55:08 +0400

    feat: provide default config values

--- tailforward-cfg/src/config.rs
+++ tailforward-cfg/src/config.rs
@@ -1,13 +1,27 @@
 #![allow(clippy::expect_used)]
 use camino::Utf8PathBuf;
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(default)]
 pub struct Config {
     pub debug: bool,
     pub tailscale_secret_file: Utf8PathBuf,
     pub telegram_secret_file: Utf8PathBuf,
     pub address: SocketAddr,
     pub chat_id: i64,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            debug: false,
+            tailscale_secret_file: "/secrets/tailscale-webhook".into(),
+            telegram_secret_file: "/secrets/telegram".into(),
+            address: SocketAddr::from_str("0.0.0.0:33010")
+                .expect("Default value for config should never panic!"),
+            chat_id: -1_001_864_190_705,
+        }
+    }
 }
