1202ca9b333d133f7d2874c786a60b3f17f111e3
KFears <kfearsoff@gmail.com>
2023-11-04 16:41:18 +0400

    docs: better config example

--- build.rs
+++ build.rs
@@ -1,9 +1,22 @@
 use camino::Utf8Path;
 use std::{error::Error, fs};
-use tailforward_cfg::Config;
+use tailforward_cfg::{
+    config::{Format, Tailscale, Telegram},
+    Config,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cfg = Config::default();
+    let cfg = Config {
+        tailscale: Tailscale {
+            secret_file: Some("/etc/tailforward/tailforward.toml".into()),
+        },
+        telegram: Telegram {
+            secret_file: Some("/secrets/telegram".into()),
+            file_format: Format::Plain,
+            chat_id: Some(-123),
+        },
+        ..Default::default()
+    };
 
     let toml_path = Utf8Path::new("examples/config.toml");
     let toml = toml::to_string(&cfg)?;
